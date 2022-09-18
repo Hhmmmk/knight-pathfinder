4ba8587368c1d67351f79711c0798bd920ff9d83
Hhmmmk <lesly.celzo@gmail.com>
2022-09-18 08:02:09 +0800

    implemented select and deselect

--- src/components/Chessboard/Chessboard.component.jsx
+++ src/components/Chessboard/Chessboard.component.jsx
@@ -1,30 +1,66 @@
+import { useRef, useState } from 'react';
+
 import Tile from '../Tile/Tile.component';
 import './Chessboard.styles.css';
+
+import knight from '../../assets/knight.png';
+import pawn from '../../assets/pawn.png';
 
 const verticalAxis = [1, 2, 3, 4, 5, 6, 7, 8];
 const horizontalAxis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
 const Chessboard = () => {
+  //--start-projectDefaults--//
   let tileInfo = [];
+  const defaultClassList = '';
+  const ref = useRef(null);
 
+  const [classList, setClassList] = useState(defaultClassList);
+  //--end-projectDefaults--//
+
+  //--start-building board--//
   for (let i = 0; i < horizontalAxis.length; i++) {
     for (let j = 0; j < verticalAxis.length; j++) {
+      //implementing alternating colors
       let tileColor = '';
       const number = i + j + 2;
-
       tileColor = number % 2 === 0 ? 'light-tiles' : 'dark-tiles';
 
+      //store tileInfo in array to be passed in the components as props
       tileInfo.push({
         tileId: horizontalAxis[i] + verticalAxis[j].toString(),
         color: tileColor,
+        classList: classList,
       });
     }
   }
+  //--end-building board--//
+
+  //--start-click handlers--//
+  const handleRightClick = (event) => {
+    event.preventDefault();
+    const activeTile = document.getElementById(event.target.id);
+    activeTile.classList.remove('selected');
+  };
+
+  const handleClick = (event) => {
+    const activeTile = document.getElementById(event.target.id);
+    activeTile.classList.add('selected');
+  };
+  //--end-click handlers--//
 
   return (
     <div className='chessboard'>
-      {tileInfo.map((tiles) => {
-        return <Tile key={tiles.tileId} info={tiles} />;
+      {tileInfo.map((tile) => {
+        return (
+          <Tile
+            ref={ref}
+            onRClickHandler={handleRightClick}
+            onClickHandler={handleClick}
+            key={tile.tileId}
+            info={tile}
+          />
+        );
       })}
     </div>
   );
--- src/components/Tile/Tile.component.jsx
+++ src/components/Tile/Tile.component.jsx
@@ -1,18 +1,39 @@
-import { useState } from 'react';
+import React, { Component } from 'react';
 
 import './Tile.styles.css';
 
-const Tile = ({ info: { tileId, color } }) => {
-  const [tileContent, setTileContent] = useState('');
+// const Tile = ({ info, onClickHandler, onRClickHandler }) => {
+//   const { tileId, color, classList } = info;
+//   // tileContent: knight, pawn
+//   // tileStatus: selected, path, possibleMove
 
-  // tileContent: knight, pawn
-  // tileStatus: selected, path, possibleMove
+//   return (
+//     <div
+//       onContextMenu={onRClickHandler}
+//       onClick={onClickHandler}
+//       id={tileId}
+//       className={`tiles ${color} ${classList}`}
+//     >
+//       <span>{`[${tileId}]`}</span>
+//     </div>
+//   );
+// };
 
-  return (
-    <div className={`tiles ${color}`}>
-      <span>{`[${tileId}]`}</span>
-    </div>
-  );
-};
+class Tile extends Component {
+  render() {
+    const { info, onClickHandler, onRClickHandler } = this.props;
+    const { tileId, color, classList } = info;
+    return (
+      <div
+        onContextMenu={onRClickHandler}
+        onClick={onClickHandler}
+        id={tileId}
+        className={`tiles ${color} ${classList}`}
+      >
+        <span>{`[${tileId}]`}</span>
+      </div>
+    );
+  }
+}
 
 export default Tile;
